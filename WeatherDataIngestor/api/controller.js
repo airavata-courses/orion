7f209c9eb7620609d275f68751e558da12b3b4c6
anbadrin <anita1397@gmail.com>
2022-02-01 16:42:50 -0500

    Add a micro service to fetch S3 data
    This microservice will get 4 valid gz files urls from AWS S3 based on radar_id, date and time

---
+++ WeatherDataIngestor/api/controller.js
@@ -0,0 +1,46 @@
+const AWS = require('aws-sdk');
+
+//init AWS S3 Bucket
+var S3 = new AWS.S3({ region: 'us-east-1' });
+
+var BucketConfig = {
+    Bucket: 'noaa-nexrad-level2',
+    Delimiter: '/',
+};
+
+
+var controllers = {
+    getS3url: function(req, res) {
+        const { params } = req;
+        const { year, month, day, hour, radar } = params;
+    
+        BucketConfig.Prefix = `${year}/${month}/${day}/${radar}/`;
+    
+        S3.makeUnauthenticatedRequest('listObjects', BucketConfig, (err, data) => {
+            if (err) {
+                res.status(500).send();
+            }
+            else {
+                const result = [];
+    
+                if (data) {
+                    const { Contents } = data;
+                    if (Contents.length) {
+                        Contents
+                            .forEach(({ Key }) => {
+                                const [, hourString] = Key.split("_");
+                                const hr = hourString.substring(0, 2);
+                                if (hr === hour) {
+                                    result.push(Key)
+                                }
+                            })
+                    }
+                }
+    
+                res.json(result.slice(0, 4))
+            }
+        })
+   },
+};
+
+module.exports = controllers;
