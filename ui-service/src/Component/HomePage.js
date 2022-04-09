9c205f885312f5f8eec654620b8ab41306ae33bf
anbadrin <anita1397@gmail.com>
2022-04-09 13:42:58 -0400

    UI code changes
    Code changes in UI to add Merra Data

--- ui-service/src/Component/HomePage.js
+++ ui-service/src/Component/HomePage.js
@@ -24,6 +24,14 @@ export default function HomePage(userData) {
     const [datacenter, setDataCenter] = useState();
     const [format, setFormat] = useState(true);
 
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [minLatitude, setMinLatitude] = useState();
+    const [maxLatitude, setMaxLatitude] = useState(); 
+    const [minLongitude, setMinLongitude] = useState();
+    const [maxLongitude, setMaxLongitude] = useState();
+    const [merraDate, setMerraDate] = useState();
+
     const [response, setResponse] = useState([]);
 
     useEffect(() => {
@@ -38,7 +46,8 @@ export default function HomePage(userData) {
     const formSubmit = async e => {
         if (e) e.preventDefault();
         //res = await sendData({date,time,datacenter,userEmail});
-        res = await sendData({ date, time, datacenter }, format);
+        if (format) res = await sendData({ date, time, datacenter }, format);
+        else res = await sendData({ username, password, minLatitude, maxLatitude, minLongitude, maxLongitude, merraDate}, format)
         setResponse(res);
         console.log(response);
     }
@@ -76,11 +85,41 @@ export default function HomePage(userData) {
                 ) : (
                     <form onSubmit={formSubmit}>
                         <label>
-                            <p><strong>Date</strong></p>
-                            <input type="date" id="date" onChange={e => setDate(e.target.value)} />
+                            <p><strong>Username</strong></p>
+                            <input type="text" id="username" onChange={e => setUsername(e.target.value)} />
                             {/* <input type="date" name="date" onChange={handleChange} />  */}
-
                         </label>
+                        <label>
+                            <p><strong>Password</strong></p>
+                            <input type="password" id="password" onChange={e => setPassword(e.target.value)} />
+                            {/* <input type="date" name="date" onChange={handleChange} />  */}
+                        </label>
+                        <label>
+                            <p><strong>Min Latitude</strong></p>
+                            <input type="text" id="minLatitude" onChange={e => setMinLatitude(e.target.value)} />
+                            {/* <input type="date" name="date" onChange={handleChange} />  */}
+                        </label>
+                        <label>
+                            <p><strong>Max Latitude</strong></p>
+                            <input type="text" id="maxLatitude" onChange={e => setMaxLatitude(e.target.value)} />
+                            {/* <input type="date" name="date" onChange={handleChange} />  */}
+                        </label>
+                        <label>
+                            <p><strong>Min Longitude</strong></p>
+                            <input type="text" id="minLongitude" onChange={e => setMinLongitude(e.target.value)} />
+                            {/* <input type="date" name="date" onChange={handleChange} />  */}
+                        </label>
+                        <label>
+                            <p><strong>Max Longitude</strong></p>
+                            <input type="text" id="maxLongitude" onChange={e => setMaxLongitude(e.target.value)} />
+                            {/* <input type="date" name="date" onChange={handleChange} />  */}
+                        </label>
+                        <label>
+                            <p><strong>Date</strong></p>
+                            <input type="date" id="merraDate" onChange={e => setMerraDate(e.target.value)} />
+                            {/* <input type="date" name="date" onChange={handleChange} />  */}
+                        </label>
+                        <button type="submit" align="center" >Diagnose Current Atmospheric Conditions </button>
                     </form>
                 )
             }
