096151e5458318968a2a8dffd1a10a2a1793fa1d
anbadrin <anita1397@gmail.com>
2022-05-01 13:26:48 -0400

    Bug Fix

---
+++ plot-service-restart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd orion/orion/
+cd plot-weather-microservice/
+kubectl delete -f plot.yaml
+kubectl apply -f plot.yaml
+cd ..
+cd merra-plot-microservice/
+kubectl delete -f plot.yaml
+kubectl apply -f plot.yaml
